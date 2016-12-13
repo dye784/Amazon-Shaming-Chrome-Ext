bc296b0c7190e3bd681d18b1554f76403bb67bec
tessaSAC <tessaSAC@users.noreply.github.com>
2016-12-13 16:36:21 -0500

    getting the total

--- scraper.js
+++ scraper.js
@@ -1,1 +1,44 @@
-alert(+($("td:contains('Total')").next().next().text()).replace(/[$,]/g, ''););
+const total = null;
+
+window.setTimeout(() => {
+	total = +(($("td:contains('Total')").next().next().text()).replace(/[$,]/g, ''));
+}, 500);
+
+
+
+// function getTotal() {
+// 	if (!total) {
+// 		window.setTimeout(() => {
+// 			total = +($("td:contains('Total')").next().next().text()).replace(/[$,]/g, '');
+// 			resolve(total);
+// 		}, 500)
+// 	}
+// }
+
+// while (!total) {
+// 	window.setTimeout(() => getTotal(), 500);
+// }
+
+
+// while (!total) {
+// 	return new Promise(function(resolve, reject) {
+// 		window.setTimeout(() => {
+// 			total = +($("td:contains('Total')").next().next().text()).replace(/[$,]/g, '');
+// 			resolve(total);
+// 		}, 500)
+// 	})
+// 	.then(() => alert("hey!"));
+// 	});
+// }
+
+
+// function findTotal() {
+// 	if (!total) {
+// 		window.setTimeout(() => {
+// 			total = +($("td:contains('Total')").next().next().text()).replace(/[$,]/g, '');
+// 		}, 500);
+// 		if (!total)
+// 	} else {
+// 		alert("hey!");
+// 	}
+// }
