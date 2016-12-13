4f4a7c58a26db5fc3bcaa047f7f18161ee2e8b37
Ian Munro <ianmunrobot@gmail.com>
2016-12-13 14:32:25 -0500

    puts selectedCharities on global scope

---
+++ selected-charities.js
@@ -0,0 +1,16 @@
+// Used to put array of user's selected charities on global scope
+
+'use strict';
+
+var selectedCharities = (function () {
+  // export value
+  var publicCharities = [];
+
+  chrome.storage.sync.get({
+    charities: []
+  }, function(items) {
+    var charities = items.charities;
+    publicCharities = charities;
+  })
+  return publicCharities;
+}());
