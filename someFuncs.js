25dbc67af834a9470c91f34eea2640bc9a4e126a
Ian Munro <ianmunrobot@gmail.com>
2016-12-13 15:39:37 -0500

    added random charity selector

--- someFuncs.js
+++ someFuncs.js
@@ -4,8 +4,17 @@ const AMERICAN_RED_CROSS = "American Red Cross";
 const SOUTHERN_POVERTY_LAW = "Southern Poverty Law Center";
 
 //simple cost calculator
-function whatYourMoneyCanDo(money, goodDeedPrice) {
+function whatYourMoneyCanDo(money, charity) {
   return money / goodDeedPrice * 1.00
+}
+
+//randomly choose a charity and return the full object
+function (charityList) {
+  var length = charityList.length;
+  var randomSelection = Math.floor(Math.random() * length);
+  var selectedCharity = charityList[randomSelection];
+  var chosenCharity = charityList[selectedCharity];
+  return chosenCharity;
 }
 
 // switch case for specific charities
@@ -28,10 +37,6 @@ function specificCharityCosts(money, charity) {
   }
 }
 
-console.log(specificCharityCosts(2000, DOCTORS_WITHOUT_BORDERS))
-console.log(specificCharityCosts(2000, WORLD_WILDLIFE_FUND))
-console.log(specificCharityCosts(2000, AMERICAN_RED_CROSS))
-console.log(specificCharityCosts(2000, SOUTHERN_POVERTY_LAW))
 
 $(function(){
   $SOMEIDHERE = $('#SOMEIDHERE');
@@ -40,19 +45,39 @@ $(function(){
     doctorsWithoutBorders: {
       name: "Doctors Without Borders",
       donationURL: 'https://donate.doctorswithoutborders.org/onetime.cfm',
-
+      resources: {
+        0: 'support the worldwide fight access to life-saving medicine',
+        0.67: 'vaccinate a child against tuberculosis, measles, diphtheria, tetanus, pertussis, and polio',
+        5: 'Give a child a rotavirus vaccine',
+        46: 'fully vaccinate a child',
+        100: 'be a hero to parients around the world'
+      }
     },
     worldWildlifeFund: {
       name: "World Wildlife Fund",
-      donationURL: 'https://support.worldwildlife.org/site/SPageServer?pagename=main_onetime&s_src=AWE1510GD914'
+      donationURL: 'https://support.worldwildlife.org/site/SPageServer?pagename=main_onetime&s_src=AWE1510GD914',
+      resources: {
+        0: 'support the worldwide fight to save animals',
+        15: 'adopt a polar bear or a penguin for a month',
+        20: 'adopt a tiber for a month',
+        25: 'adopt an endangered animal'
+      }
     },
     americanRedCross: {
       name: "American Red Cross",
-      donationURL: 'https://www.redcross.org/donate/donation'
+      donationURL: 'https://www.redcross.org/donate/donation',
+      resources: {
+        0: 'help distribute life-saving blood units worldwide',
+        250: 'pay for the total costs of a unit of blood',
+      }
     },
     southernPoveryLawCenter: {
       name: "Southern Poverty Law Center",
-      donationURL: 'https://donate.splcenter.org/'
+      donationURL: 'https://donate.splcenter.org/',
+      resources: {
+        0: 'have your donation matched dollar-for-dollar to support civil rights',
+        50: 'have over 50% of your gift go directly to public education and the fight for civil rights'
+      }
     }
   }
 
