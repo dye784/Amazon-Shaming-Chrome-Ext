b01b70ca5dd284333ea5ed5fa59c5dfbafbd8d33
tessaSAC <tessaSAC@users.noreply.github.com>
2016-12-13 16:40:12 -0500

    Scraper (#1)
    * nightmare scraper
    
    * add content script
    
    * getting the total

---
+++ scraper.js
@@ -0,0 +1,44 @@
+const total = null;
+
+window.setTimeout(() => {
+	total = +(($("td:contains('Total')").next().next().text()).replace(/[$,]/g, ''));
+}, 500);
+
+
+
+// function getTotal() {
+// 	if (!total) {
+// 		window.setTimeout(() => {
+// 			total = +($("td:contains('Total')").next().next().text()).replace(/[$,]/g, '');
+// 			resolve(total);
+// 		}, 500)
+// 	}
+// }
+
+// while (!total) {
+// 	window.setTimeout(() => getTotal(), 500);
+// }
+
+
+// while (!total) {
+// 	return new Promise(function(resolve, reject) {
+// 		window.setTimeout(() => {
+// 			total = +($("td:contains('Total')").next().next().text()).replace(/[$,]/g, '');
+// 			resolve(total);
+// 		}, 500)
+// 	})
+// 	.then(() => alert("hey!"));
+// 	});
+// }
+
+
+// function findTotal() {
+// 	if (!total) {
+// 		window.setTimeout(() => {
+// 			total = +($("td:contains('Total')").next().next().text()).replace(/[$,]/g, '');
+// 		}, 500);
+// 		if (!total)
+// 	} else {
+// 		alert("hey!");
+// 	}
+// }
